7188462b83e936d099a27751a3f39429f2822330
MonsileI <hansaem900d@kakao.com>
2023-12-19 20:51:43 +0900

    2023-12-19 : 4문제 알고리즘 1. 그래프 - 3문제 2. 그리디 -2문제
    느낀점 : 소프티어 시험을 앞두고 다양한 문제를 풀었다. 기분이 좋다.

---
+++ src/전체문제/Sixth_2023_12_19/st_수퍼바이러스_Level_3.java
@@ -0,0 +1,27 @@
+package 전체문제.Sixth_2023_12_19;
+import java.util.*;
+import java.io.*;
+public class st_수퍼바이러스_Level_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=  new StringTokenizer(br.readLine()," ");
+        long K = Long.parseLong(st.nextToken());
+        long P = Long.parseLong(st.nextToken());
+        long N = Long.parseLong(st.nextToken()) * 10;
+        //1000000007
+
+        long result = calc(P,N);
+        System.out.println(result*K % 1000000007);
+    }
+    static long calc(long p,long n){
+        if(n==1) return p;
+
+        long cur = calc(p,n/2);
+
+        if(n%2==1){
+            return (cur * cur % 1000000007) * p % 1000000007;
+        }else{
+            return cur * cur % 1000000007;
+        }
+    }
+}
---
+++ src/전체문제/Sixth_2023_12_19/st_우물안개구리_Level_3.java
@@ -0,0 +1,44 @@
+package 전체문제.Sixth_2023_12_19;
+import java.util.*;
+import java.io.*;
+public class st_우물안개구리_Level_3 {
+    static class Node implements Comparable<Node>{
+        int to; int weight;
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return -(weight - o.weight);
+        }
+    }
+    static int[]powers;
+    static List<PriorityQueue<Node>> list;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=  new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        powers = new int[N+1];
+        list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=1;i<N+1;i++)powers[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N+1;i++) list.add(new PriorityQueue<>());
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int a= Integer.parseInt(st.nextToken());
+            int b= Integer.parseInt(st.nextToken());
+            list.get(a).offer(new Node(b,powers[b]));
+            list.get(b).offer(new Node(a,powers[a]));
+        }
+        int ans = 0;
+        for(int i=1;i<N+1;i++){
+            if(list.get(i).isEmpty()) ans++;
+            else if(powers[i]>list.get(i).peek().weight)ans++;
+        }
+        System.out.println(ans);
+    }
+}
