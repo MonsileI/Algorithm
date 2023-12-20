2aa4644fdb8dc4b1d7cba4065af8bf30a5d8e2ba
MonsileI <hansaem900d@kakao.com>
2023-12-21 05:13:42 +0900

    2023-12-21 : 2문제 알고리즘 1. DP - 2문제
    느낀점 : 넵섹이 두가지 유형으로 나뉘는데, 물건을 많이 넣거나 하나만 넣을 수 있는 경우에 대해 그림을 그려 학습하였다.

---
+++ src/전체문제/Seventh_2023_12_21/bj_22115_창영이와커피_Gold_5.java
@@ -0,0 +1,32 @@
+package 전체문제.Seventh_2023_12_21;
+import java.util.*;
+import java.io.*;
+public class bj_22115_창영이와커피_Gold_5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[]arr =new int[N+1];
+        st= new StringTokenizer(br.readLine()," ");
+        for(int i=1;i<N+1;i++)arr[i] = Integer.parseInt(st.nextToken());
+        int[][]dp = new int[N+1][M+1];
+        int INF = 987654321;
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<M+1;j++){
+                dp[i][j] = INF;
+            }
+        }
+        dp[0][0] = 0;
+        for(int i=1;i<N+1;i++){
+            int caf = arr[i];
+            for(int j=0;j<M+1;j++){
+                dp[i][j] = dp[i-1][j];
+                if(caf<=j){
+                    dp[i][j] = Math.min(dp[i][j],dp[i-1][j-caf]+1);
+                }
+            }
+        }
+        System.out.println(dp[N][M]==INF ? -1 : dp[N][M]);
+    }
+}
---
+++ src/전체문제/Seventh_2023_12_21/bj_2631_줄세우기_Gold_4.java
@@ -0,0 +1,21 @@
+package 전체문제.Seventh_2023_12_21;
+import java.util.*;
+import java.io.*;
+public class bj_2631_줄세우기_Gold_4 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[]arr = new int[N];
+        for(int i=0;i<N;i++)arr[i] = Integer.parseInt(br.readLine());
+        int[]dp = new int[N];
+        int max = 0;
+        for(int i=0;i<N;i++){
+            dp[i] = 1;
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]) dp[i] = Math.max(dp[i],dp[j]+1);
+            }
+            max = Math.max(max,dp[i]);
+        }
+        System.out.println(N-max);
+    }
+}
