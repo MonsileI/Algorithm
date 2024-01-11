c299fcf1b2f8c0a7cfc55866fe2e29630e7f7e08
MonsileI <hansaem900d@kakao.com>
2024-01-11 18:15:41 +0900

    2024-01-11 : 1 문제 알고리즘 : 1. LIS - 1 문제
    느낀점 : LIS 응용 복습

--- src/DP/LIS/ST_징검다리_Level_3.java
+++ src/DP/LIS/ST_징검다리_Level_3.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class st_징검다리_Level_3 {
+public class ST_징검다리_Level_3 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
---
+++ src/전체문제/second_month_2024_01/eigth_2024_01_11/BJ_2565_전깃줄_Gold_5.java
@@ -0,0 +1,30 @@
+package 전체문제.second_month_2024_01.eigth_2024_01_11;
+import java.util.*;
+import java.io.*;
+public class BJ_2565_전깃줄_Gold_5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = null;
+        int[][]arr = new int[N][2];
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr,(o1, o2) -> o1[0]-o2[0]);
+        int ans = 0;
+        int[]lis = new int[N];
+        for(int i=0;i<N;i++){
+            lis[i] = 1;
+            for(int j=0;j<i;j++){
+                if(arr[j][1]<arr[i][1]){
+                    lis[i] = Math.max(lis[i],lis[j]+1);
+                }
+            }
+            ans = Math.max(ans,lis[i]);
+        }
+        System.out.println(N-ans);
+
+    }
+}
