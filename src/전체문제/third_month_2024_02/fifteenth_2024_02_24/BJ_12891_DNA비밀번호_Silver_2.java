9a1288d80e6ca09dcdb5b0c678829f6f333d0436
MonsileI <hansaem900d@kakao.com>
2024-02-24 17:36:47 +0900

    2024-02-24 : 3 문제 알고리즘 : 1. 투포인터 - 1문제 2. 이분탐색 - 1문제 3.그리디 -1문제
    느낀점 : 그리디 문제 재밌다.

---
+++ src/전체문제/third_month_2024_02/fifteenth_2024_02_24/BJ_12891_DNA비밀번호_Silver_2.java
@@ -0,0 +1,118 @@
+package 전체문제.third_month_2024_02.fifteenth_2024_02_24;
+import java.util.*;
+import java.io.*;
+public class BJ_12891_DNA비밀번호_Silver_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        //길이가 M이어야 하고,
+        int[]arr = new int[20]; //A 0 C 2 G 6 T 19  || A B C D E F G H I J K L M N O P Q R S T
+        int[] idx = {0,2,6,19};
+        String str = br.readLine();
+        StringBuilder sb= new StringBuilder();
+        int len = N;
+        for(int i=0;i<N;i++){
+            char ch = str.charAt(i);
+            if(ch!='A' && ch!='C' && ch!='G' &&ch!='T'){
+                len--;
+                continue;
+            }
+            sb.append(ch);
+        }
+        N = len;
+        str = sb.toString();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<4;i++) arr[idx[i]] = Integer.parseInt(st.nextToken());
+        int L = 0;
+        int R = 1;
+        int ans = 0;
+        if(N<2){
+            int firIdx = str.charAt(0)-'A';
+            boolean flag = true;
+            for(int i=0;i<4;i++){
+                if(firIdx==idx[i]){
+                    if(firIdx<idx[i]) {
+                        flag = false;
+                        break;
+                    }
+                }else{
+                    if(arr[idx[i]]>0) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if(!flag) System.out.println(0);
+            else System.out.println(1);
+        }else if(M==1){
+            boolean flag = true;
+            int checkIdx  = -1;
+            for(int i=0;i<4;i++){
+                if(arr[idx[i]]>1){
+                    flag = false;
+                    checkIdx = 100;
+                    break;
+                }
+                if(arr[idx[i]]==1){
+                    if(flag){
+                        checkIdx = idx[i];
+                        flag = false;
+                    }else{
+                        checkIdx = 100;
+                        flag =false;
+                        break;
+                    }
+                }
+            }
+            if(checkIdx==100) System.out.println(0);
+            else{
+                if(checkIdx==-1) ans = N;
+                else {
+                    for(int i=0;i<N;i++) if(str.charAt(i)-'A'==checkIdx)ans++;
+                }
+                System.out.println(ans);
+            }
+        }
+        else {
+            //마이너스면 넘치는거,
+            arr[str.charAt(L) - 'A']--;
+            arr[str.charAt(R) - 'A']--;
+            while (true) {
+                if ((R - L + 1) == M) break;
+                R++;
+                arr[str.charAt(R) - 'A']--;
+            }
+            boolean flag = true;
+            for (int i = 0; i < 4; i++) {
+                if (arr[idx[i]] > 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            OuterLoop:
+            while (true) {
+                if (flag) {
+                    ans++;
+                    if (arr[str.charAt(L) - 'A'] == 0) flag = false;
+                }
+                arr[str.charAt(L) - 'A']++;
+                R++;
+                if (R == N) break;
+                arr[str.charAt(R) - 'A']--;
+                L++;
+                boolean small = true;
+                for (int i = 0; i < 4; i++) {
+                    if (arr[idx[i]] > 0) {
+                        small = false;
+                        break;
+                    }
+                }
+                if (small) flag = true;
+            }
+            System.out.println(ans);
+        }
+    }
+}
+
---
+++ src/전체문제/third_month_2024_02/fifteenth_2024_02_24/BJ_1911_흙길보수하기_Gold_5.java
@@ -0,0 +1,50 @@
+package 전체문제.third_month_2024_02.fifteenth_2024_02_24;
+import java.util.*;
+import java.io.*;
+public class BJ_1911_흙길보수하기_Gold_5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int L = 0 ; int R = M+1;
+        int[][]arr= new int[N][2];
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken())-1;
+        }
+        Arrays.sort(arr,(o1,o2)->o1[0]-o2[0]);
+            int tmp  = 0;
+            int len = arr[0][1]-arr[0][0]+1;
+            int before = 0;
+            while(true){
+                if(len<=0)  break;
+                len -= M;
+                before += M;
+                tmp++;
+            }
+            for(int i=1;i<N;i++){
+                if(before<arr[i][0]) {
+                    before = arr[i][0];
+                    len = arr[i][1] - arr[i][0]+1;
+                    while(true){
+                        if(len<=0) break;
+                        len -= M;
+                        before += M;
+                        tmp++;
+                    }
+                    before--;
+                }else if(before<arr[i][1]){
+                    len = arr[i][1] - before;
+                    while(true){
+                        if(len<=0) break;
+                        len -= M;
+                        before += M;
+                        tmp++;
+                    }
+                }
+            }
+            System.out.println(tmp);
+    }
+}
