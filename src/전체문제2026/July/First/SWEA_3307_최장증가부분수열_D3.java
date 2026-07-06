c2fa1740469b484d833cce3760d58463690d1eaf
monsileI <hansaem900@kakao.com>
2026-07-06 15:02:40 +0900

    2026-07-04 : 1 문제 - 알골ì고리즘 1. DP : 1 문제

---
+++ src/전체문제2026/July/First/SWEA_3307_최장증가부분수열_D3.java
@@ -0,0 +1,31 @@
+package 전체문제2026.July.First;
+
+import java.util.*;
+import java.io.*;
+public class SWEA_3307_최장증가부분수열_D3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb= new StringBuilder();
+        int TC = Integer.parseInt(br.readLine());
+        for(int t=1;t<=TC;t++){
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine()," ");
+            int[]arr = new int[N];
+            for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+            int[]dp = new int[N];
+            int answer = 1;
+            for(int i=0;i<N;i++){
+                dp[i] = 1;
+                for(int j=0;j<i;j++){
+                    if(arr[j]<arr[i]){
+                        dp[i] = Math.max(dp[i],dp[j]+1);
+                    }
+                }
+                answer = Math.max(answer,dp[i]);
+            }
+            sb.append("#"+t+" "+answer+"\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
