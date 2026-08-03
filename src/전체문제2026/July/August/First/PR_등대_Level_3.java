bce2da89884f9f6705751cbb6ec9c09ca9f70a76
monsileI <hansaem900@kakao.com>
2026-08-03 17:18:56 +0900

    2026-08-03 : 3 문제 - 알고리즘 1. DFS - 1 문제 2. 당ê다익스트라 - 1 문제 3. 그ë¦구현 - 1문제

---
+++ src/전체문제2026/July/August/First/PR_등대_Level_3.java
@@ -0,0 +1,40 @@
+package 전체문제2026.July.August.First;
+
+import java.util.*;
+
+public class PR_등대_Level_3 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[][]lighthouse = {{4, 1}, {5, 1}, {5, 6}, {7, 6}, {1, 2}, {1, 3}, {6, 8}, {2, 9}, {9, 10}};
+        System.out.println(solution(n,lighthouse));
+    }
+    static List<List<Integer>> list;
+    static int answer;
+    static boolean[]visited;
+    static int solution(int n, int[][] lighthouse) {
+        list = new ArrayList<>();
+        for(int i=0;i<=n;i++) list.add(new ArrayList<>());
+        for(int i=0;i<lighthouse.length;i++){
+            list.get(lighthouse[i][0]).add(lighthouse[i][1]);
+            list.get(lighthouse[i][1]).add(lighthouse[i][0]);
+        }
+        visited = new boolean[n+1];
+        dfs(0,1);
+        return answer;
+    }
+    static int dfs(int before, int cur){
+        visited[cur] = true;
+        if(list.get(cur).size()==1 && list.get(cur).get(0)==before) return 1;
+        int tmp = 0;
+        for(int next : list.get(cur)){
+            if(!visited[next]) {
+                tmp += dfs(cur, next);
+            }
+        }
+        if(tmp!=0){
+            answer++;
+            return 0;
+        }
+        return 1;
+    }
+}
---
+++ src/전체문제2026/July/August/First/PR_롤케이크자르기_Level_2.java
@@ -0,0 +1,34 @@
+package 전체문제2026.July.August.First;
+
+import java.util.*;
+public class PR_롤케이크자르기_Level_2 {
+    public static void main(String[] args) {
+        int[]topping = {1,2,3,1,2,3,1,2};
+        System.out.println(solution(topping));
+    }
+    static int solution(int[] topping) {
+        int answer = 0;
+        int N = topping.length;
+        int sum = 0;
+        int[] rightArr = new int[10001];
+        for(int i=0;i<N;i++){
+            if(rightArr[topping[i]]==0) sum++;
+            rightArr[topping[i]]++;
+        }
+        int left = 0;
+        int right = sum;
+        int[]leftArr = new int[10001];
+        for (int i = 0; i < N; i++) {
+            if(left==right) answer++;
+            rightArr[topping[i]]--;
+            if (rightArr[topping[i]] == 0) {
+                right--;
+            }
+            if(leftArr[topping[i]]==0){
+                left++;
+            }
+            leftArr[topping[i]]++;
+        }
+        return answer;
+    }
+}
