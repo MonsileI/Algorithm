cb4ed69b0b9327950c597b3e0857f4cf8c35c125
monsileI <hansaem900@kakao.com>
2026-08-21 16:58:51 +0900

    2026-08-21 : 6문제 - 알고리즘 1. 이분탐색 : 3문제 2. 트라이 : 1문제 3. 구현 : 1 2문제

---
+++ src/전체문제2026/July/August/Fourteenth/GR_감시초소배치_D3.java
@@ -0,0 +1,37 @@
+package 전체문제2026.July.August.Fourteenth;
+
+import java.util.*;
+import java.io.*;
+public class GR_감시초소배치_D3 {
+    static int N;
+    static int[]arr;
+    static int M;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for(int i=0;i<N;i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        int L = 0; int R = arr[N-1] - arr[0];
+        while(L<R){
+            int mid = (L+R)/2;
+            if(calc(mid)<M) R = mid;
+            else L = mid+1;
+        }
+        System.out.println(L);
+    }
+    static int calc(int mid){
+        int cnt = 1;
+        int before = arr[0];
+        for(int i=1;i<N;i++){
+            if(mid < arr[i] - before){
+                before = arr[i];
+                cnt++;
+            }
+            if(M<cnt) return cnt;
+        }
+        return cnt;
+    }
+}
---
+++ src/전체문제2026/July/August/Fourteenth/GR_파테크_D3.java
@@ -0,0 +1,36 @@
+package 전체문제2026.July.August.Fourteenth;
+
+import java.util.*;
+import java.io.*;
+public class GR_파테크_D3 {
+    static int N,M;
+    static int[]arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine()," ");
+        int L = 0; int R = 0;
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            R = Math.max(R,arr[i]);
+        }
+        while(L<R){
+            int mid = (L+R)/2;
+            if(calc(mid)<M) R = mid;
+            else L = mid+1;
+        }
+        System.out.println(L-1);
+    }
+    static long calc(int mid){
+        long sum = 0;
+        for(int i=0;i<N;i++){
+            if(mid<arr[i]) {
+                sum += arr[i] - mid;
+            }
+        }
+        return sum;
+    }
+}
